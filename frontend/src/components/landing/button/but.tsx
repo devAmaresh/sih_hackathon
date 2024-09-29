284f5554d80157db7d713eda8688462b33b8e090
devAmaresh <amprasad.2003@gmail.com>
2024-09-30 00:42:30 +0530

    minor

--- frontend/src/components/landing/button/but.tsx
+++ frontend/src/components/landing/button/but.tsx
@@ -2,9 +2,9 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 
 const but = ({ text }: any) => {
   return (
-    <button className="flex bg-black text-white rounded-md space-x-2 p-1.5 items-center">
+    <button className="flex bg-black text-white space-x-2 p-1.5 items-center">
       <div className="ml-2 text-sm">{text}</div>
-      <div className="bg-white rounded-md p-1 px-2">
+      <div className="bg-white p-1 px-2">
         <IoIosArrowRoundForward className="w-6 h-6 text-black" />
       </div>
     </button>
