f4bdc9cde78fcd9a4f809a07e565591bb982023d
devAmaresh <amprasad.2003@gmail.com>
2024-10-02 00:11:18 +0530

    ui

--- frontend/src/components/landing/head.tsx
+++ frontend/src/components/landing/head.tsx
@@ -2,17 +2,21 @@ const head = () => {
   return (
     <div
       className="relative flex flex-col justify-center items-center h-screen bg-cover bg-center"
-      style={{
-        backgroundImage: 'url("/images/head.jpg")',
-        objectFit: "cover",
-      }}
+      // style={{
+      //   backgroundImage: 'url("/images/head.jpg")',
+      //   objectFit: "cover",
+      // }}
     >
-      <div className="text-pretty md:text-5xl text-4xl font-semibold font-serif text-[#e0e0e0]">
-        <div className="text-center pb-3">Explore a Completely New</div>
-        <div className="text-center">Museum Experience</div>
+      <div className="text-pretty md:text-6xl text-4xl font-semibold font-serif text-black px-2 md:px-0">
+        <div className="text-center bg-gradient-to-r from-yellow-500 to-pink-500 bg-clip-text text-transparent  md:text-6xl text-4xl font-bold pb-3">
+          Explore a Completely New
+        </div>
+        <div className="pb-3 text-center bg-gradient-to-r from-yellow-500 to-pink-500 bg-clip-text text-transparent  md:text-6xl text-4xl font-bold">
+          Museum Experience
+        </div>
       </div>
       <div className="flex justify-center">
-        <div className="lg:w-[650px] pt-10 text-center mx-4 md:mx-0 text-[#ffffff]">
+        <div className="lg:w-[650px] pt-7 text-center mx-4 md:mx-0 text-black">
           Welcome to an exhilarating voyage through the Vietnam Museum of
           Ethnology, where Vietnamese history and culture come alive through
           innovative AR technology and gamification, creating an immersive and
