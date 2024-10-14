2f5e0e92c57f4934149dd69979778e145a7b7afd
devAmaresh <amprasad.2003@gmail.com>
2024-10-14 21:15:58 +0530

    fix

--- frontend/src/utils/backend_url.tsx
+++ frontend/src/utils/backend_url.tsx
@@ -1,3 +1,1 @@
-const debug = import.meta.env.DEBUG;
-export const backendUrl =
-  debug === "true" ? "http://localhost:8000" : import.meta.env.VITE_BACKEND_URL;
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
