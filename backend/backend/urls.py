5e91bf8c04f22ae255d177b769cd71a39f9c25b1
devAmaresh <amprasad.2003@gmail.com>
2024-10-14 21:22:26 +0530

    added home

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.http import HttpResponse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("sih.urls")),
+    path("", lambda request: HttpResponse("Welcome to the backend of the SIH project")),
 ]
